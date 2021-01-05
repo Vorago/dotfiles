4deed4967c076ab60f6c8379a7a009c2fdad2fe0
Andrew Naydyonock <a.naydyonock@bill.me>
2021-01-05 22:21:44 +0200

    zsh plugins, chrome, jq

---
+++ zsh/install-plugins.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
